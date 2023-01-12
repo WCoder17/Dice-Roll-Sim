c135faee5b713fa028c3b694197203f7e9e209f4
willCodesInBinary <112642024+WCoder17@users.noreply.github.com>
2023-01-12 09:58:05 -0700

    Progress

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Welcome to Dice Roll Sim
+Console.Clear();
+
+#nullable disable
+Console.WriteLine("Dice Sim Menu");
+Console.WriteLine("1:Roll Dice Once");
+Console.WriteLine("2:Roll Dice 5 Times");
+Console.WriteLine("3:Roll Dice 'n' times");
+Console.WriteLine("4:Roll Dice until Snake Eyes");
+Console.WriteLine("5:Exit");
+// Main Loop
+while (true)
+{
+    // Dice Sim Menu
+    Console.Write("\nSelect an option (1-5): ");
+    string roll = Console.ReadLine();
+
+    // Process Input
+    if (roll == "1") {
+        Console.WriteLine("1 is working");
+    } else if (roll != "1"){
+        Console.WriteLine("1 is working but was not chosen");
+        
+    } else if (roll == "5") {
+        break;
+    }
+}
